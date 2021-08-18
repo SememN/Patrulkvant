334467244ff0ac8c49a7f51267bae2c1a5905d6a
Sam <51040158+SememN@users.noreply.github.com>
2021-08-18 15:48:02 +0500

    Update accepter.py

--- backend/routing/accepter.py
+++ backend/routing/accepter.py
@@ -24,6 +24,3 @@ def json_to_dict(file_path):
 
 def get_current_coords(coords_dict, coords_to_return: int):
     return coords_dict['coords'][coords_to_return]
-
-
-print(check_file('asdfsaf.file'))
