30d15de26f30faea7819ee536408652b20e62f19
Sam <51040158+SememN@users.noreply.github.com>
2021-08-18 15:48:33 +0500

    fix bug

--- backend/routing/rotating.py
+++ backend/routing/rotating.py
@@ -6,12 +6,12 @@ import gps_coords_parser
 
 def calc_distance(start: dict, point: dict): 
     """calculates amount of a vector from start to point2""" 
-    return sqrt((point['x']-start['x'])**2+(point['y']-start['y'])**2) 
+    return sqrt((point['lng']-start['lng'])**2+(point['lat']-start['lat'])**2)
  
  
 def calc_cos(distance, start: dict, point: dict): 
     """calculates cosine of right triangle in unit semicircle""" 
-    cosine = (point['y'] - start['y']) / distance
+    cosine = (point['lat'] - start['lat']) / distance
     return cosine 
  
  
@@ -23,31 +23,30 @@ def calc_degrees(cosine):
  
 def detect_side(start: dict, point: dict, degrees): 
     """detect to which side robot should rotate""" 
-    if start['y'] < point['y'] and start['x'] < point['x']: 
+    if start['lat'] < point['lat'] and start['lng'] < point['lng']:
         return f'{degrees} degrees right' 
-    elif start['y'] < point['y'] and start['x'] > point['x']: 
+    elif start['lat'] < point['lat'] and start['lng'] > point['lng']:
         return f'{degrees} degrees left' 
-    elif start['y'] > point['y'] and start['x'] < point['x']: 
+    elif start['lat'] > point['lat'] and start['lng'] < point['lng']:
         return f'{degrees + 90} degrees right' 
-    elif start['y'] > point['y'] and start['x'] > point['x']: 
+    elif start['lat'] > point['lat'] and start['lng'] > point['lng']:
         return f'{degrees + 90} degrees left' 
     elif degrees == 0: 
         return f'{0} degress' 
     elif degrees == 180: 
         return f'{180} degrees right' 
-    elif start['y'] == point['y'] and start['x'] < point['x']: 
+    elif start['lat'] == point['lat'] and start['lng'] < point['lng']:
         return f'{degrees} degress right' 
-    elif start['y'] == point['y'] and start['x'] > point['x']: 
+    elif start['lat'] == point['lat'] and start['lng'] > point['lng']:
         return f'{degrees} degress left' 
  
- 
-json_coords = open('coords.json', 'r') 
-coords_dict = accepter.json_to_dict(json_coords)
+
+coords_dict = accepter.json_to_dict('coords.json')
  
 start_coords = gps_coords_parser.get_coords('$GPRMC,104725.00,A,5509.10700,N,06124.20465,E,6.149,337.86,020821,,,A*6A')
  
  
-# cycle to get all rotations for the robot 
+# cycle to get all rotations for the robot
 for i in range(7): 
     current_coords = accepter.get_current_coords(coords_dict, i)
     distance = calc_distance(start_coords, current_coords) 
