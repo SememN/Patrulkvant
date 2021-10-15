1726080df6f7ba057a414df352f7351792adc1c8
Sam <51040158+SememN@users.noreply.github.com>
2021-10-15 18:53:26 +0500

    Delete get_gps_coords.py

--- backend/routing/arduino_controller/get_gps_coords.py
+++
@@ -1,8 +0,0 @@
-import serial
-
-
-def read_coords(arduino: serial.Serial):
-    while True:
-        line = arduino.readline().decode('utf-8')
-        if line.startswith('$GNRMC'):
-            return line
