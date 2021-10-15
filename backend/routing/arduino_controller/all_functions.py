272b0de927e011881c487f24876cff28b29a89e3
Sam <51040158+SememN@users.noreply.github.com>
2021-10-15 19:44:13 +0500

    update syntax

--- backend/routing/arduino_controller/all_functions.py
+++ backend/routing/arduino_controller/all_functions.py
@@ -2,37 +2,35 @@ import serial
 
 
 class Functions(object):
-    #clean the serial port in arduino
-    def clean_serial(self, arduino: serial.Serial):
-        arduino.write('clean')
+    def __init__(self):
+        # you should define this var in main file
+        self.arduino: serial.Serial()
+        self.arduino = None
+
+    # clean the serial port in arduino
+    def clean_serial(self):
+        self.arduino.write('clean')
         return
 
-    #initializing the gyroscope
-    def read_gyroscope(self, arduino: serial.Serial):
+    # initializing the gyroscope
+    def read_gyroscope(self):
         while True:
-            line = arduino.readline().decode('utf-8')
+            line = self.arduino.readline().decode('utf-8')
             if line.startswith('gyroscope: '):
                 return line.split(':')[1][1:]
 
-    #get coordinates from GPS
-    def read_coords(self, arduino: serial.Serial):
+    # get coordinates from GPS
+    def read_coords(self):
         while True:
-            line = arduino.readline().decode('utf-8')
+            line = self.arduino.readline().decode('utf-8')
             if line.startswith('$GNRMC'):
                 return line
 
-    #part of riding to arduino
-    def drive(self, arduino: serial.Serial):
-        arduino.write('drive')
+    # part of riding to arduino
+    def drive(self):
+        self.arduino.write('drive')
+        return 'asd'
+
+    def stop(self):
+        self.arduino.write('stop')
         return
-
-    def stop(self, arduino: serial.Serial):
-        arduino.write('stop')
-        return
-
-
-
-
-
-
-
